ae4a1e755e7835880c11fa8b7a82b06ef362a0f2
yanbin_yb <yanbin.yb@hotmail.com>
2023-11-30 12:06:06 +0000

    update lib/fulltext.py.
    Signed-off-by: yanbin_yb <yanbin.yb@hotmail.com>

--- lib/fulltext.py
+++ lib/fulltext.py
@@ -1,59 +1,136 @@
-
-from flask import Flask, app, request
-from loguru import logger
-import json
-
-from lib.fulltext import FullText
-
-
-def responseOk(data=None):
-    result = {}
-    result["code"] = 0
-    if data is not None:
-        result["data"] = data
-    return result
+import jieba
+import datetime
+from sqlalchemy import and_, create_engine
+from sqlalchemy import DateTime, Text, VARCHAR
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import sessionmaker
 
 
-def responseErr(data=None):
-    result = {}
-    result["code"] = -1
-    if data is not None:
-        result["data"] = data
-    return result
+class Base(DeclarativeBase):
+    pass
+
+class KeyToId(Base):
+    __tablename__ = "t_key_to_id"
+    key: Mapped[str] = mapped_column(VARCHAR(50), primary_key=True)
+    ids: Mapped[str] = mapped_column(Text())
 
 
-app = Flask(__name__)
+class IdToKey(Base):
+    __tablename__ = "t_id_to_key"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    keys: Mapped[str] = mapped_column(Text())
+    content: Mapped[str] = mapped_column(VARCHAR(200))
+    created_time: Mapped[DateTime] = mapped_column(
+        DateTime(), default=datetime.datetime.now)
+    modified_time: Mapped[DateTime] = mapped_column(
+        DateTime(), default=datetime.datetime.now)
 
 
-@app.route('/index', methods=['POST'])
-def createIndex():
-    data = json.loads(request.get_data(as_text=True))
-    if data["source"] is None or request.args.get("source") == "":
-        return responseErr("source不能为空")
-    if data["id"] is None or data["id"] == "":
-        return responseErr("id不能为空")
-    if data["content"] is None or data["content"] == "":
-        return responseErr("content不能为空")
-    fulltext = FullText(data["source"])
-    fulltext.createIndex(data["id"], data["content"])
-    return responseOk()
+dbpool = dict()
 
+class FullText:
+    def __init__(self, source: str):
+        if dbpool.get(source):
+            self.engine = dbpool[source]
+            Base.metadata.create_all(self.engine)
+        else:
+            self.engine = create_engine("sqlite:///"+source+".db", echo=False)
+            dbpool[source] = self.engine
+            Base.metadata.create_all(self.engine)
 
-@app.route('/search', methods=['GET'])
-def query():
-    if request.args.get("source") is None or request.args.get("source") == "":
-        return responseErr("source不能为空")
-    if request.args.get("keys") is None or request.args.get("keys") == "":
-        return responseErr("keys")
-    fulltext = FullText(request.args.get("source"))
-    keys = request.args.get("keys").split(",")
-    result = fulltext.query(keys)
-    return responseOk(result)
+    def _listostr(self, mylist: list):
+        if mylist is None:
+            return ''
+        str = ''
+        index = 0
+        while len(mylist) > 0:
+            temp = mylist.pop(index)
+            if len(mylist) == 0:
+                str = str + temp
+            else:
+                str = str+temp+","
+        return str
 
+    def _merge(self, temp: list, list: list):
+        result = []
+        for id in list:
+            if temp.count(id) > 0:
+                result.append(id)
+        return result
 
-if __name__ == '__main__':
-    try:
-        logger.add("./http.log")
-        app.run(host='0.0.0.0', port=8080)
-    except Exception as err:
-        logger.error("err %s: " % err)
+    def clear(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        session.query(KeyToId).delete()
+        session.query(IdToKey).delete()
+        session.commit()
+
+    def createIndex(self, id: str, txt: str):
+        cutkey = jieba.cut_for_search(txt)
+        keys = ''
+        lkeys = []
+        for key in cutkey:
+            keys = keys + key+","
+            lkeys.append(key)
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        idtokey = session.query(IdToKey).filter_by(id=id).first()
+        if idtokey is None:
+            idtokey = IdToKey(id=id, keys=keys, content=txt)
+            session.add(idtokey)
+        else:
+            idtokey.modified_time = datetime.datetime.now()
+            keylist = idtokey.keys.split(',')
+            idtokey.keys = keys
+            idtokey.content = txt
+            keytoids = session.query(KeyToId).filter(
+                KeyToId.key.in_(keylist)).all()
+            if len(keytoids) > 0:
+                for keytoid in keytoids:
+                    myids = keytoid.ids.split(",")
+                    myids.remove(id)
+                    if len(myids) == 0:
+                        session.delete(keytoid)
+                    else:
+                        keytoid.ids = self._listostr(myids)
+
+        keytoids = session.query(KeyToId).filter(
+            KeyToId.key.in_(lkeys)).all()
+        for keytoid in keytoids:
+            if lkeys.count(keytoid.key) > 0:
+                lkeys.remove(keytoid.key)
+            list = keytoid.ids.split(',')
+            if list.count(id) == 0:
+                list.append(id)
+                keytoid.ids = self._listostr(list)
+        if len(lkeys) > 0:
+            for key in lkeys:
+                keytoid = KeyToId(key=key, ids=id)
+                session.add(keytoid)
+        session.commit()
+
+    def query(self, keys: list):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        ids = None
+        for key in keys:
+            if key == "":
+                continue
+            keytoid = session.query(KeyToId).filter_by(key=key).first()
+            if keytoid is None:
+                return None
+            else:
+                temp = keytoid.ids.split(",")
+                if ids == None:
+                    ids = temp
+                else:
+                    ids = self._merge(ids, temp)
+                    if len(ids) == 0:
+                        return None
+        idtokeys = session.query(IdToKey).filter(IdToKey.id.in_(ids)).all()
+        result = []
+        for idtokey in idtokeys:
+            result.append({"id": idtokey.id, "content": idtokey.content})
+        return result
