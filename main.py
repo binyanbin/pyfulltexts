8ee873cea30309b22e53f55fdf8e4cdf139a253b
yanbin_yb <yanbin.yb@hotmail.com>
2023-12-01 02:22:40 +0000

    update main.py.
    Signed-off-by: yanbin_yb <yanbin.yb@hotmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
-from flask import Flask, app, request
-from loguru import logger
-import json
-
+import asyncio
+import tornado
 from lib.fulltext import FullText
 
 
@@ -22,38 +20,66 @@ def responseErr(data=None):
     return result
 
 
-app = Flask(__name__)
+def containsKeys(obj, keys):
+    if isinstance(obj, dict):
+        return all(key in obj.keys() for key in keys)
+    elif hasattr(type(obj), '__dict__'):
+        return all(key in obj.__dict__ for key in keys)
+    else:
+        return False
 
 
-@app.route('/index', methods=['POST'])
-def createIndex():
-    data = json.loads(request.get_data(as_text=True))
-    if data["source"] is None or request.args.get("source") == "":
-        return responseErr("source不能为空")
-    if data["id"] is None or data["id"] == "":
-        return responseErr("id不能为空")
-    if data["content"] is None or data["content"] == "":
-        return responseErr("content不能为空")
-    fulltext = FullText(data["source"])
-    fulltext.createIndex(data["id"], data["content"])
-    return responseOk()
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        source = self.get_query_argument("source", "")
+        keys = self.get_query_argument("keys", "")
+        if source == "":
+            self.write(responseErr("source不能为空"))
+            return
+        if keys == "":
+            self.write(responseErr("keys不能为空"))
+            return
+        fulltext = FullText(source)
+        lkeys = keys.split(",")
+        result = fulltext.query(lkeys)
+        self.write(responseOk(result))
+
+    def post(self):
+        data = tornado.escape.json_decode(self.request.body)
+        if containsKeys(data, ["source", "id", "content"]) != True:
+            self.write(responseErr("source不能为空"))
+            return
+        if data["source"] == "":
+            self.write(responseErr("source不能为空"))
+            return
+        if data["id"] == "":
+            self.write(responseErr("id不能为空"))
+            return
+        if data["content"] == "":
+            self.write(responseErr("content不能为空"))
+            return
+        fulltext = FullText(data["source"])
+        if containsKeys(data, ["keys"]) and len(data["keys"]) > 0:
+            fulltext.createIndex2(
+                data["id"], data["keys"], data["content"])
+        else:
+            fulltext.createIndex(id=data["id"], txt=data["content"])
+        self.write(responseOk())
 
 
-@app.route('/search', methods=['GET'])
-def query():
-    if request.args.get("source") is None or request.args.get("source") == "":
-        return responseErr("source不能为空")
-    if request.args.get("keys") is None or request.args.get("keys") == "":
-        return responseErr("keys")
-    fulltext = FullText(request.args.get("source"))
-    keys = request.args.get("keys").split(",")
-    result = fulltext.query(keys)
-    return responseOk(result)
+port = 8888
 
 
-if __name__ == '__main__':
-    try:
-        logger.add("./http.log")
-        app.run(host='0.0.0.0', port=8080)
-    except Exception as err:
-        logger.error("err %s: " % err)
+def make_app():
+    return tornado.web.Application([
+        (r"/index", MainHandler),
+    ])
+
+
+async def main():
+    app = make_app()
+    app.listen(port)
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
